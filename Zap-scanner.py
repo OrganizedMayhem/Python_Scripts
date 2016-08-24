1d7d8c6420934927e2b36a0d89aa428128e4f127
Silver Evans <Silver.evans@aspect.com>
2016-08-24 17:43:18 -0400

    Changes

--- Zap-scanner.py
+++ Zap-scanner.py
@@ -1,4 +1,4 @@
-import time, collections, prettytable, zapv2,
+import time, collections, prettytable, zapv2
 from zapv2 import ZAPv2
 from collections import defaultdict
 from time import sleep
@@ -21,9 +21,12 @@ target = raw_input('URL to Scan? ')
 
 # Variables used in Authentication (HTTP/NTLM Authentication)
 port = raw_input('Port? ')
+
+# Request Username and Password for Application login
 username = raw_input('Username? ')
 password = raw_input('Password? ')
 
+# Configure Authentication
 print ''
 print 'Select an Authentication Method: '
 print ''
@@ -34,25 +37,27 @@ AuthenticationMethod = input('Input Line Item from Above: ')
 
 # Input Validation (Checks for Valid selection)
 if AuthenticationMethod == 1:
-    print ''
-    print 'Setting Authentication Method to Form-Based Authentication'
-    print ''
-    zap.authentication.set_authentication_method(contextid=1, authmethodname='formBasedAuthentication',
-                                                 authmethodconfigparams=('loginURL=' + target + '&username=' + username
-                                                                         + '&password=' + 'password'))
+	print ''
+	print 'Setting Authentication Method to Form-Based Authentication'
+	print ''
+	zap.authentication.set_authentication_method(contextid=1, authmethodname='formBasedAuthentication',
+												 authmethodconfigparams=('loginURL=' + target + '&username=' + username
+																		 + '&password=' + 'password'))
 elif AuthenticationMethod == 2:
-    print ''
-    print 'Setting Authentication Method to httpAuthentication'
+	print ''
+	print 'Setting Authentication Method to httpAuthentication'
 
-    zap.authentication.set_authentication_method(contextid=1, authmethodname='httpAuthentication',
-                                                 authmethodconfigparams=('hostname=' + target + '&port=' + port),
-                                                 apikey=key)
+	zap.authentication.set_authentication_method(contextid=1, authmethodname='httpAuthentication',
+												 authmethodconfigparams=('hostname=' + target + '&port=' + port),
+												 apikey=key)
+else:
+	AuthenticationMethod = input(prompt='Invalid Entry...Try Again.')
 
 print 'Adding User ' + username + ' to Context.'
 zap.users.new_user(contextid=1, name=username, apikey=key)
 zap.users.set_authentication_credentials(contextid=1, userid=0, authcredentialsconfigparams=('username=' + username +
-                                                                                             '&password=' + password),
-                                         apikey=key)
+																							 '&password=' + password),
+										 apikey=key)
 print ''
 print 'Enabling user ' + username
 zap.users.set_user_enabled(contextid=1, userid=0, enabled=True, apikey=key)
@@ -68,8 +73,8 @@ print 'Spider Status print'
 
 # Prints Spider status percentage every 1 second (Not working)
 while zap.spider.status < 100:
-    print zap.spider.status
-    time.sleep(1)
+	print zap.spider.status
+	time.sleep(1)
 
 print '100 Complete'
 
@@ -78,8 +83,8 @@ time.sleep(5)
 print 'Start Active Scanning'
 zap.ascan.scan(url=target, recurse=False, inscopeonly=True, apikey=key)
 while (int(zap.ascan.status)) < 100:
-    print zap.ascan.status
-    time.sleep(1)
+	print zap.ascan.status
+	time.sleep(1)
 
 print '100 Complete'
 print ''
@@ -88,17 +93,16 @@ time.sleep(5)
 print 'Do you want to save your session?'
 Save = raw_input('y/n').lower()
 if Save == 'y':
-    SessionName = raw_input('Name of Session?')
-    zap.core.save_session(name=SessionName, overwrite=False, apikey=key)
-    print 'Session has been saved to C:\Users\{username}\OWASP ZAP\session'
-    zap.core.shutdown(self, apikey=key)
+	SessionName = raw_input('Name of Session?')
+	zap.core.save_session(name=SessionName, overwrite=False, apikey=key)
+	print 'Session has been saved to C:\Users\{username}\OWASP ZAP\session'
+	zap.core.shutdown(self, apikey=key)
 elif Save == 'n':
-    print "You've decided to not save your session."
-    print 'Shutting Down ZAP in 10 Seconds..'
-    
-    x = 10
-    while x != 0:
-        print x
-        threading.sleep(1)
-        x -= 1
-    zap.core.shutdown(self, apikey=key)
+	print "You've decided to not save your session."
+	print 'Shutting Down ZAP in 10 Seconds..'
+	x = 10
+	while x != 0:
+		print x
+		threading.sleep(1)
+		x -= 1
+	zap.core.shutdown(self, apikey=key)
